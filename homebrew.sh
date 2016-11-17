6e2edb9d8700946ee8272ea01092f84db8a64616
Sebastian Nemak <s7nio@users.noreply.github.com>
2016-11-17 14:47:58 +0100

    installation check

--- homebrew.sh
+++ homebrew.sh
@@ -3,12 +3,23 @@
 # install xcode cli tools
 xcode-select --install
 
-# install homebrew in ~ path
-mkdir ${HOME}/Homebrew
-git clone https://github.com/Homebrew/brew ${HOME}/Homebrew
-#export PATH=~/Homebrew/bin:${PATH}
+# install ~/Homebrew
+homebrewDir="${HOME}/Homebrew"
+if [ -d "$homebrewDir" ]
+then
+	echo "Homebrew ($homebrewDir) is already installed"
+else
+  echo "Installed Homebrew in $homebrewDir"
+  mkdir "$homebrewDir"
+  git clone https://github.com/Homebrew/brew ${HOME}/Homebrew
+  #export PATH=~/Homebrew/bin:${PATH}
+	echo "... installed"
+  echo "... change $homebrewDir access permissions"
+  chmod -R 700 ${HOME}/Homebrew
+  echo "... check Homebrew installation..."
+  brew doctor
+fi
+
+echo "Done"
 
 # un/install (https://github.com/Homebrew/install)
-
-# change access permissions
-chmod -R 700 ${HOME}/Homebrew
