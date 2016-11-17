38ddefe870a1cb8d5acc90624fcda4887b977bea
Sebastian Nemak <s7nio@users.noreply.github.com>
2016-11-17 14:33:06 +0100

    cleanup comments

--- homebrew.sh
+++ homebrew.sh
@@ -5,18 +5,8 @@ xcode-select --install
 
 # install homebrew in ~ path
 mkdir ${HOME}/Homebrew
-#cd ${HOME}/Homebrew
-#git init
-#git remote add origin https://github.com/Homebrew/brew
-#git fetch
-#git checkout origin/master
 git clone https://github.com/Homebrew/brew ${HOME}/Homebrew
 #export PATH=~/Homebrew/bin:${PATH}
-
-# defaults
-#HOMEBREW_PREFIX: /usr/local
-#HOMEBREW_REPOSITORY: /usr/local/Homebrew
-#HOMEBREW_CELLAR: /usr/local/Cellar
 
 # un/install (https://github.com/Homebrew/install)
 
