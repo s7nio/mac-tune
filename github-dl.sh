15d0804f2a3ded8ecd42cb51136b909b3462b6f1
Sebastian Nemak <s7nio@users.noreply.github.com>
2016-11-19 20:51:21 +0100

    add github repo download script

---
+++ github-dl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+gitConfigFile="$HOME/.gitconfig"
+if [[ ! -f "$gitConfigFile" ]];
+then
+   echo "File $gitConfigFile does not exist. Please setup git."
+   exit -1
+else
+   echo "File $gitConfigFile exist."
+   githubUser=$(git config --global github.user)
+   if [[ ! -n $githubUser ]];
+   then
+     echo "Missing 'github.user' configuration."
+     exit -1
+   else
+     dir=$(pwd)"/github"
+     if [[ -d $dir ]];
+     then
+       echo "$dir already exist."
+     else
+       mkdir -p $dir
+       echo "$dir created."
+     fi
+     cd $dir
+     #https://gist.github.com/caniszczyk/3856584
+     clone="curl -s https://api.github.com/users/$githubUser/repos?per_page=100 | python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system(\"git clone \" + repo[\"ssh_url\"])'"
+     eval $clone
+   fi
+fi
+echo "Done"
